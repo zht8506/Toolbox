808a61d469003af278b640c5794a06cc2e1e0e7c
zht8506 <81303574+zht8506@users.noreply.github.com>
2025-03-25 11:31:37 +0800

    Create lora.py

---
+++ lora.py
@@ -0,0 +1,40 @@
+from huggingface_hub import snapshot_download
+
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# load base model
+model_name = "xxx_llm_dir"
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# load pre-trained lora weight
+lora_weight = "xxx_lora_dir"
+model = PeftModel.from_pretrained(model, lora_weight)
+
+# a simple infer code using llm
+prompt = "Give me a short introduction to large language model."
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
