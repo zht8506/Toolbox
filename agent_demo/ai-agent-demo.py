1c9b610e489abf8578c82766381d931694cf713c
zht8506 <81303574+zht8506@users.noreply.github.com>
2025-05-25 20:44:17 +0800

    Create ai-agent-demo.py

---
+++ agent_demo/ai-agent-demo.py
@@ -0,0 +1,68 @@
+# from https://gist.github.com/cradiator/3be43011060abb6869f4b41f0a9ce165
+
+#### main.py
+from pydantic_ai.models.gemini import GeminiModel
+from pydantic_ai import Agent
+
+from dotenv import load_dotenv
+import tools
+
+load_dotenv()
+model = GeminiModel("gemini-2.5-flash-preview-04-17")
+
+agent = Agent(model,
+              system_prompt="You are an experienced programmer",
+              tools=[tools.read_file, tools.list_files, tools.rename_file])
+
+def main():
+    history = []
+    while True:
+        user_input = input("Input: ")
+        resp = agent.run_sync(user_input,
+                              message_history=history)
+        history = list(resp.all_messages())
+        print(resp.output)
+
+
+if __name__ == "__main__":
+    main()
+
+
+#### tools.py
+from pathlib import Path
+import os
+
+base_dir = Path("./test")
+
+
+def read_file(name: str) -> str:
+    """Return file content. If not exist, return error message.
+    """
+    print(f"(read_file {name})")
+    try:
+        with open(base_dir / name, "r") as f:
+            content = f.read()
+        return content
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+def list_files() -> list[str]:
+    print("(list_file)")
+    file_list = []
+    for item in base_dir.rglob("*"):
+        if item.is_file():
+            file_list.append(str(item.relative_to(base_dir)))
+    return file_list
+
+def rename_file(name: str, new_name: str) -> str:
+    print(f"(rename_file {name} -> {new_name})")
+    try:
+        new_path = base_dir / new_name
+        if not str(new_path).startswith(str(base_dir)):
+            return "Error: new_name is outside base_dir."
+
+        os.makedirs(new_path.parent, exist_ok=True)
+        os.rename(base_dir / name, new_path)
+        return f"File '{name}' successfully renamed to '{new_name}'."
+    except Exception as e:
+        return f"An error occurred: {e}"
